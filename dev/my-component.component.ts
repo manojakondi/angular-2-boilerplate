157c0a6873ae192b2c64ae13fabc05a6fab9802e
Manoj Akondi <apnmanoj4u@gmail.com>
2016-05-15 13:09:29 +0530

    in-built directives
    used in-built style and disabled directives

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -3,10 +3,12 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-component',
     template: `
-        Heyaa! I'm {{name}} and this is my first angular2 app. <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!</span>
+        Heyaa! I'm <span [style.color]="inputElement.value === 'yes' ? 'orange':''">{{name}}</span> and this is my first angular2 app. <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!</span>
         <br><br>
         Is it awesome?
         <input type="text" required #inputElement (keyup)="0">
+        <br><br>
+        <button [disabled]="inputElement.value !== 'yes'">Only enabled if 'yes' was entered</button>
     `,
     styleUrls: ['src/css/mycomponent.css']
 })
