fdfa5014ceb8a45d55529265f991519dac1f93be
Manoj Akondi <apnmanoj4u@gmail.com>
2016-05-15 12:56:05 +0530

    basic styling and forms and events
    added a custom scss file and a very basic form and attached an keyup
    event

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -2,9 +2,15 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-component',
-    template: 'This is second component'
+    template: `
+        Heyaa! I'm {{name}} and this is my first angular2 app. <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!</span>
+        <br><br>
+        Is it awesome?
+        <input type="text" required #inputElement (keyup)="0">
+    `,
+    styleUrls: ['src/css/mycomponent.css']
 })
 
 export class MyComponentComponent {
-
+    name="Manoj";
 }
