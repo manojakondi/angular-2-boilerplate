db9218988f229a243f0dba26ca58e5947bda2d24
Manoj Akondi <apnmanoj4u@gmail.com>
2016-05-15 12:01:59 +0530

    Added my-component
    Learned to create a new component and embedding it.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,14 @@
 import {Component} from 'angular2/core';
+import {MyComponentComponent} from './my-component.component';
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h2>Here comes another component</h2>
+        <my-component></my-component>
     `,
+    directives: [MyComponentComponent]
 })
 export class AppComponent {
 
---
+++ dev/my-component.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-component',
+    template: 'This is second component'
+})
+
+export class MyComponentComponent {
+
+}
